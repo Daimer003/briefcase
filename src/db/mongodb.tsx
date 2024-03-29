e0d250ddf21d519d7c976a3dbe8cd0b36aea86b7
Daymer - Developer <daymer-developer@MacBook-Pro-de-Daymer.local>
2024-03-29 18:10:33 -0500

    feat: url

--- src/db/mongodb.tsx
+++ src/db/mongodb.tsx
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 const user = process.env.NEXT_PUBLIC_OPENAI_USER
 const pass = process.env.NEXT_PUBLIC_OPENAI_PASS
 
-
 export async function connectDb() {
-    await mongoose.connect(`mongodb+srv://${user}:${pass}@comments.qur7yse.mongodb.net/?retryWrites=true&w=majority&appName=comments`)
+    await mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.vffoffq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 }
 
