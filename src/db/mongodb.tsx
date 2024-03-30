134897bd3660cc7433bab4b68819bc5b8ef2a92d
Daymer - Developer <daymer-developer@MacBook-Pro-de-Daymer.local>
2024-03-29 20:25:27 -0500

    feat: url mongo

--- src/db/mongodb.tsx
+++ src/db/mongodb.tsx
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-//*const developer = "mongodb://localhost:27017/"
+//const developer = "mongodb://localhost:27017/"
 const user = process.env.NEXT_PUBLIC_OPENAI_USER
 const pass = process.env.NEXT_PUBLIC_OPENAI_PASS
 
