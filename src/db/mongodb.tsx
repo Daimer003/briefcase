f5900841ad457875e0dc1be28540a0e6f223d7dc
daimer003 <daimer003@gmail.com>
2024-09-05 17:37:50 -0500

    fix: env

--- src/db/mongodb.tsx
+++ src/db/mongodb.tsx
@@ -4,5 +4,5 @@ const developer = "mongodb://localhost:27017/";
 const url = process.env.NEXT_PUBLIC_URL_MONGODB;
 
 export async function connectDb() {
-  await mongoose.connect(String(developer));
+  await mongoose.connect(String(url));
 }
